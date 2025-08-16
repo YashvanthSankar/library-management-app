8058d5a89093c280047229823bccd74a2f206487
Yashvanth S <yashvanthsankar@gmail.com>
2025-08-16 08:42:28 +0530

    add shadcn components

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  console.log("Library Management API is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
